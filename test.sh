1ed8cace78f3b7a11127b81e4a924e0716f43356
fujitani sora <fujitanisora0414@gmail.com>
2025-11-25 22:14:34 +0900

    nitpik

--- test.sh
+++
@@ -1,4 +0,0 @@
-rfmt 0.1.0 built to pkg/rfmt-0.1.0.gem.
-Tag v0.1.0 has already been created.
-Pushing gem to https://rubygems.org...
-There was a problem saving your gem: Name 'rfmt' is too similar to an existing gem named 'rformat'
